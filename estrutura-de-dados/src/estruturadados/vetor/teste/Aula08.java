e62505d2e49a024f8bb2d70e6be6f940d26270ec
Wagner-Kazuhiko <kazu_hiko@hotmail.com>
2022-11-14 18:03:13 -0300

    add estrutura de dados eclipse

---
+++ estrutura-de-dados/src/estruturadados/vetor/teste/Aula08.java
@@ -0,0 +1,22 @@
+package estruturadados.vetor.teste;
+
+import estruturadados.vetor.Vetor;
+
+public class Aula08 {
+
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(3);
+		
+		vetor.adiciona("B");
+		vetor.adiciona("C");
+		vetor.adiciona("D");
+		vetor.adiciona("E");
+		vetor.adiciona("F");
+		vetor.adiciona("G");
+		
+		System.out.println(vetor);
+
+	}
+
+}
