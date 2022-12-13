1d4b114c996cb8e5404516bb7fe84b24f5921301
Wagner-Kazuhiko <kazu_hiko@hotmail.com>
2022-12-12 22:10:08 -0300

    create class triangle

--- nelio-poo-organizado-intellij/curso-java-nelio/src/application/Program.java
+++ nelio-poo-organizado-intellij/curso-java-nelio/src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import entities.Triangle;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -7,22 +9,25 @@ public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner scan = new Scanner(System.in);
-        double xA, xB, xC, yA, yB, yC;
+
+        Triangle x, y;
+        x = new Triangle();
+        y = new Triangle();
 
         System.out.println("Enter the measures of triangle X: ");
-        xA = scan.nextDouble();
-        xB = scan.nextDouble();
-        xC = scan.nextDouble();
+        x.a = scan.nextDouble();
+        x.b = scan.nextDouble();
+        x.c = scan.nextDouble();
         System.out.println("Enter the the measures of triangle Y:  ");
-        yA = scan.nextDouble();
-        yB = scan.nextDouble();
-        yC = scan.nextDouble();
+        y.a = scan.nextDouble();
+        y.b = scan.nextDouble();
+        y.c = scan.nextDouble();
 
-        double p = (xA + xB + xC) / 2.0;
-        double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+        double p = (x.a + x.b + x.c) / 2.0;
+        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
 
-        p = (yA + yB + yC) / 2.0;
-        double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+        p = (y.a + y.b + y.c) / 2.0;
+        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
 
         System.out.printf("Triangle area X: %.4f%n", areaX );
         System.out.printf("Triangle area y: %.4f%n", areaY );
