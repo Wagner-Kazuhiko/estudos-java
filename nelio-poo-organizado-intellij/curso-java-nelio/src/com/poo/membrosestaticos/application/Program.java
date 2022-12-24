a2c39a756f0b3d402a6c6dbe55d84a01e1345e56
Wagner Kazuhiko <kazu_hiko@hotmail.com>
2022-12-24 20:32:59 -0300

    opp static members

--- nelio-poo-organizado-intellij/curso-java-nelio/src/com/poo/membrosestaticos/application/Program.java
+++ nelio-poo-organizado-intellij/curso-java-nelio/src/com/poo/membrosestaticos/application/Program.java
@@ -1,11 +1,11 @@
 package com.poo.membrosestaticos.application;
 
+import com.poo.membrosestaticos.util.Calculator;
+
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
-
-    public static final double PI = 3.14159;
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner scan = new Scanner(System.in);
@@ -13,24 +13,14 @@ public class Program {
         System.out.println("Enter radius: ");
         double radius = scan.nextDouble();
 
-        double c = circumference(radius);
+        double c = Calculator.circumference(radius);
 
-        double v = volume(radius);
+        double v = Calculator.volume(radius);
 
         System.out.printf("Circumference: %.2f%n", c);
         System.out.printf("Volume: %.2f%n", v);
-        System.out.printf("PI value: %.2f%n", PI);
+        System.out.printf("PI value: %.2f%n", Calculator.PI);
 
         scan.close();
     }
-
-    public static double circumference(double radius){
-        return 2.0 * PI * radius;
-    }
-
-    public static double volume(double radius){
-        return 4.0 * PI * radius * radius * radius / 3;
-    }
-
-
 }
