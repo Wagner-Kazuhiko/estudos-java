92eb2c0f0bd3e5eaadf7bd2d4b57c1e989f1da9f
Wagner Kazuhiko <kazu_hiko@hotmail.com>
2022-12-27 18:43:12 -0300

    java exercise vect poo class Rent

---
+++ nelio-poo-organizado-intellij/exercicioPOO/exerciciosPOO/src/com/exercises/vetor/exercise1/application/Program.java
@@ -0,0 +1,43 @@
+package com.exercises.vetor.exercise1.application;
+
+import com.exercises.vetor.exercise1.entities.Rent;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+        Rent[] vect = new Rent[10];
+
+        System.out.println("How many rooms will be rented? ");
+        int n = scan.nextInt();
+
+        for (int i = 1; i <= n; i++){
+            System.out.println("Rent #" + i + ":");
+            System.out.print("Name: ");
+            scan.nextLine();
+            String name = scan.nextLine();
+            System.out.print("Email: ");
+            String email = scan.next();
+            System.out.print("Room: ");
+            int roomNumber = scan.nextInt();
+
+            vect[roomNumber] = new Rent(name, email);
+        }
+
+        System.out.println();
+        System.out.println("Busy rooms: ");
+        for (int i = 0; i < 10; i++){
+            if (vect[i] != null){
+                System.out.println(i + ": " + vect[i]);
+            }
+
+        }
+
+
+        scan.close();
+    }
+}
---
+++ nelio-poo-organizado-intellij/exercicioPOO/exerciciosPOO/src/com/exercises/vetor/exercise2/application/Program.java
@@ -0,0 +1,8 @@
+package com.exercises.vetor.exercise2.application;
+
+public class Program {
+    public static void main(String[] args) {
+
+
+    }
+}
