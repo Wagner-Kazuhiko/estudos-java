63a1188fc2d32a8668793909650172122eeba9c7
Wagner Kazuhiko <kazu_hiko@hotmail.com>
2022-12-27 15:31:02 -0300

    java vector part 2

---
+++ nelio-poo-organizado-intellij/curso-java-nelio/src/com/poo/vetor2/application/Program.java
@@ -0,0 +1,34 @@
+package com.poo.vetor2.application;
+
+import com.poo.vetor2.entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        Product[] vect = new Product[n];
+
+        for (int i = 0; i < vect.length; i++){
+            scan.nextLine();
+            String name = scan.nextLine();
+            double price = scan.nextDouble();
+            vect[i] = new Product(name, price);
+        }
+
+        double sum = 0;
+        for(int i = 0; i < vect.length; i++){
+            sum += vect[i].getPrice();
+        }
+
+        double avg = sum / n;
+
+        System.out.printf("AVERAGE PRICE = %.2f%n" , avg);
+
+        scan.close();
+    }
+}
