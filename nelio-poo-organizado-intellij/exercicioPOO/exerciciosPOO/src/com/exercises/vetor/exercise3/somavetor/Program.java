734bab0cd10f31373c53ceaa4d9598f13ae8631a
Wagner Kazuhiko <kazu_hiko@hotmail.com>
2022-12-27 21:58:30 -0300

    java exercise vect poo sum vectors

--- nelio-poo-organizado-intellij/exercicioPOO/exerciciosPOO/src/com/exercises/vetor/exercise3/somavetor/Program.java
+++ nelio-poo-organizado-intellij/exercicioPOO/exerciciosPOO/src/com/exercises/vetor/exercise3/somavetor/Program.java
@@ -13,5 +13,28 @@ public class Program {
 
         int[] vect = new int[n];
 
+        for (int i = 0; i < n; i++){
+            System.out.print("Enter number " + (i + 1) + ": ");
+            System.out.println();
+            vect[i] = scan.nextInt();
+        }
+
+        System.out.print("VALUES: ");
+        for (int i = 0; i < n; i++){
+            System.out.print(vect[i] + " ");
+        }
+
+        System.out.println();
+        double sum = 0.0;
+        for (int i = 0; i < n; i++){
+            sum += vect[i];
+        }
+
+        System.out.print("SOMA: " + sum);
+
+        double avg = sum / n;
+
+        System.out.println();
+        System.out.println("AVERAGE: " + avg);
     }
 }
