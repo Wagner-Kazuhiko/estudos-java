4808f40c31f0208f6662739a62e2b0e133d3069c
Wagner Kazuhiko <kazu_hiko@hotmail.com>
2023-01-03 20:44:42 -0300

    exercise matriz

---
+++ nelio-poo-organizado-intellij/exercicioPOO/exerciciosPOO/src/com/exercises/matrizes/exercise1/application/Program.java
@@ -0,0 +1,39 @@
+package com.exercises.matrizes.exercise1.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int [][] mat = new int[n][n];
+
+        for (int i = 0; i < mat.length; i++){
+            for (int j = 0; j < mat.length; j++){
+                mat[i][j] = scan.nextInt();
+            }
+        }
+
+        System.out.println("Main diagonal: ");
+        for (int i = 0; i < mat.length; i++){
+            System.out.print(mat[i][i] + " ");
+        }
+        System.out.println();
+
+        int count = 0;
+        for (int i = 0; i < mat.length; i++){
+            for (int j= 0; j < mat.length; j++){
+                if (mat[i][j] < 0){
+                    count++;
+                }
+            }
+        }
+
+        System.out.println("Negative numbers = " + count);
+
+        scan.close();
+    }
+}
