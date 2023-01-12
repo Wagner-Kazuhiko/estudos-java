fd366bf56e0d65a8971fdbc70e8536f281f20720
Wagner-Kazuhiko <kazu_hiko@hotmail.com>
2023-01-12 18:50:20 -0300

    java poo heranca

---
+++ nelio-poo-organizado-intellij/curso-java-nelio/src/com/poo/heranca/application/Program.java
@@ -0,0 +1,12 @@
+package com.poo.heranca.application;
+
+import com.poo.heranca.entities.BusinessAccount;
+
+public class Program {
+    public static void main(String[] args) {
+
+        BusinessAccount account = new BusinessAccount();
+
+        account.getBalance();
+    }
+}
