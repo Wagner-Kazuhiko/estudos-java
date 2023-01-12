01d4a1f49c0108133405045162b9976913133fbb
Wagner-Kazuhiko <kazu_hiko@hotmail.com>
2023-01-11 21:33:11 -0300

    exercise2 stringBuilder

---
+++ nelio-poo-organizado-intellij/curso-java-nelio/src/com/poo/enums/exercicioresolvido2/stringbuilder/entities/Comment.java
@@ -0,0 +1,20 @@
+package com.poo.enums.exercicioresolvido2.stringbuilder.entities;
+
+public class Comment {
+
+    private String text;
+
+    public Comment(){}
+
+    public Comment(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
