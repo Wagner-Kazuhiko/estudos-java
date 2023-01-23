f08482918d6040dff5d53330933d69bb1f45468b
Wagner-Kazuhiko <kazu_hiko@hotmail.com>
2023-01-23 20:23:27 -0300

    aula 05 estrutura de dados obter elemento de uma posicao

---
+++ estrutura-dados-java-loiane-intellij/src/com/kazu/estruturadados/vetor/teste/Aula05.java
@@ -0,0 +1,16 @@
+package com.kazu.estruturadados.vetor.teste;
+
+import com.kazu.estruturadados.vetor.Vetor;
+
+public class Aula05 {
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(10);
+
+        vetor.adiciona("elemento 1");
+        vetor.adiciona("elemento 2");
+        vetor.adiciona("elemento 3");
+
+        System.out.println(vetor.busca(1));
+    }
+}
