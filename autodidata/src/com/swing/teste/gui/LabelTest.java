3a8a946aedc1d62171ba026e17a121b1d6b7a0ce
Wagner-Kazuhiko <kazu_hiko@hotmail.com>
2023-03-08 20:49:19 -0300

    teste jLabel swing

---
+++ autodidata/src/com/swing/teste/gui/LabelTest.java
@@ -0,0 +1,15 @@
+package com.swing.teste.gui;
+
+import javax.swing.*;
+
+public class LabelTest {
+    public static void main(String[] args) {
+
+        //Testando Label Frame
+
+        LabelFrame labelframe = new LabelFrame();
+        labelframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        labelframe.setSize(260, 180);
+        labelframe.setVisible(true);
+    }
+}
