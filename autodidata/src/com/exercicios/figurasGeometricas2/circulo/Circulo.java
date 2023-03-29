caade46139fe8331a0bc6e357bf3ceb4ce391543
Wagner-Kazuhiko <kazu_hiko@hotmail.com>
2023-03-29 20:06:25 -0300

    atualizacao

---
+++ autodidata/src/com/exercicios/figurasGeometricas2/circulo/Circulo.java
@@ -0,0 +1,17 @@
+package com.exercicios.figurasGeometricas2.circulo;
+
+public class Circulo extends Ponto{
+
+    private float raio;
+
+    public Circulo(int x, int y, float raio){
+        super(x, y);
+        this.raio = raio;
+    }
+
+    @Override
+    public boolean igual(Ponto p){
+        Circulo c = (Circulo) p;
+        return super.igual(p) && this.raio == c.raio;
+    }
+}
---
+++ autodidata/src/com/exercicios/figurasGeometricas2/circulo/Ponto.java
@@ -0,0 +1,13 @@
+package com.exercicios.figurasGeometricas2.circulo;
+
+public class Ponto {
+    private int x, y;
+    public Ponto(int x , int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public boolean igual(Ponto p){
+        return this.x == p.x && this.y == p.y;
+    }
+}
