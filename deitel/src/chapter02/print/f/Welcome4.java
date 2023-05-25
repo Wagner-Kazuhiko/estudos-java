e5d241357eec33f1387c8032db4d243cbe32ea77
Wagner-Kazuhiko <kazu_hiko@hotmail.com>
2023-05-25 20:55:54 -0300

    add diretorio Deitel

---
+++ deitel/src/chapter02/print/f/Welcome4.java
@@ -0,0 +1,9 @@
+package chapter02.print.f;
+
+public class Welcome4 {
+
+    public static void main(String[] args) {
+
+        System.out.printf("%s %n %s %n", "Welcome to ", "Java Programming!");
+    }
+}
